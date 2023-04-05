f37150f376642a65b17710c63bf62a69584eaa93
Excutier <madhulucky4676@gmail.com>
2023-04-05 14:33:03 +0530

    delete and Edit functionality

--- demo.js
+++ demo.js
@@ -1,1 +1,25 @@
-console.log('improve Yourself')
+var form = document.getElementById('addform');
+var itemList = document.getElementById('items');
+form.addEventListener('submit',addItem);
+function addItem(e){
+    e.preventDefault();
+    var newItem = document.getElementById('item').value ;
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+    itemList.list.appendChild(li);
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteBtn);
+    itemList.appendChild(li);
+}
+function removeItem(e){
+    if(e.target.classList.contains(delete)){
+        if(confirm('Are you Sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+            
+        }
+    }
+}
